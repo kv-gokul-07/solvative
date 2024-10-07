6d4ef30a986ab7afe38495ee158af63c872ea569
Gokul V <84239942+kv-gokul-07@users.noreply.github.com>
2024-10-07 13:29:09 +0530

    Add files via upload
    adding script in commonSearch component for input field focus

--- src/components/commonSearch/CommonSearch.jsx
+++ src/components/commonSearch/CommonSearch.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from "./CommonSearch.module.css";
 
 const CommonSearch = ({handleFilterChange}) => {
 
   const [searchData, setSearchData] = useState("");
 
+  const inputField = useRef();
+
+  useEffect(() => {
+
+    const handleKeyDown = (event) => {
+      if(event.key === "/") {
+        inputField.current.focus();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  })
+
   const handleChange = (event) => {
     const newValue = event.target.value;
     setSearchData(newValue);
-    handleFilterChange(newValue)
+    handleFilterChange(newValue);
   }
+
   return (
     <div className={styles.searchFieldWrapper}>
-          <input type="text" name="CountryFilter" placeholder='Search Places' onChange={handleChange} value={searchData}  className={styles.textField}/>
+          <input ref={inputField} type="text" name="CountryFilter" placeholder='Search Places' onChange={handleChange} value={searchData}  className={styles.textField}/>
       <div className={styles.ctrlCmd}>Ctrl + /</div>
     </div>
   )
